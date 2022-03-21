5c4aa2632c078478b7c1387571a18a990eba80ce
Yamika reddy <Yamika reddy@DESKTOP-NUUHF54>
2022-03-21 16:38:54 +0530

    Save Programs

---
+++ Example/src/com/Basic/AdditionExample.java
@@ -0,0 +1,13 @@
+package com.Basic;
+
+public class AdditionExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 5, b = 4,c;
+		c = a + b;
+		System.out.println("Sum is :"+c);
+
+	}
+
+}
